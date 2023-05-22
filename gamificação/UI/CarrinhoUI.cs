2647822ad013d73e96f3dfb3706ccfc72416b08f
danielcasanova12 <snaxofc10@gmail.com>
2023-05-22 14:42:29 -0300

    alter

--- gamificação/UI/CarrinhoUI.cs
+++ gamificação/UI/CarrinhoUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gamificacao.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace gamificacao.UI
 {
-    internal class CarrinhoUI
+    public class CarrinhoUI
     {
+
+
+        public void AdicionarProdutoAoCarrinho(Estoque estoque,CarrinhoDeCompras carrinho,int tipo)
+        {
+            while (true)
+            {
+
+                 //List<Produto> filtrados = estoque.ListarProdutos(tipo);
+                List<ProdutoModel> filtrados = estoque.ListarProdutos(tipo);
+                foreach (var pro in filtrados)
+                {
+                    decimal valorTotalItem = pro.Preco - pro.Desconto;
+                    Console.WriteLine($"{pro.Codigo} - {pro.Nome} ({pro.Preco:C2} - {pro.Desconto:C2} = {valorTotalItem:C2})");
+                }
+                estoque.ListarProdutos(tipo);
+                Console.WriteLine("Digite o código do produto que deseja adicionar ao carrinho (ou 0 para sair):");
+                int codigo = int.Parse(Console.ReadLine());
+                if (codigo == 0)
+                {
+                    break;
+                }
+                var produto = estoque.AdicionarProdutoNoCarrinho(codigo, tipo);
+                if (produto != null)
+                {
+                    carrinho.AdicionarProduto(produto);
+                }
+                else
+                {
+                    Console.WriteLine("Produto não encontrado no estoque.");
+                }
+            }
+        }
     }
 }
